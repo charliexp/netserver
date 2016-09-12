78a1816da1d6558638c791901578631ca9c3f502
charlie_weng <charliexp.weng@gmail.com>
2016-09-12 18:52:46 +0800

    add Signed-off-by: charlie_weng <charliexp.weng@gmail.com>

---
+++ loginserver/mqtt-rpc/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var Server = require('./lib/server.js');
+var Client = require('./lib/client.js');
+
+exports.server = function(mqttclient){
+  return new Server(mqttclient);
+};
+
+exports.client = function(mqttclient){
+  return new Client(mqttclient);
+};
+
+exports.codecs = function(){
+  return require('./lib/codecs.js');
+};
