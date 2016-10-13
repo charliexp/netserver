5a7bb4cb938b61e37e06c9b47f1d04253b7a64c0
charlie_weng <charliexp.weng@gmail.com>
2016-10-13 10:12:18 +0800

    add dbapi.js
    Signed-off-by: charlie_weng <charliexp.weng@gmail.com>

---
+++ devdb/dbapi.js
@@ -0,0 +1,88 @@
+/*************************************************************************\
+ * File Name    : dbApi.js                                               *
+ * --------------------------------------------------------------------- *
+ * Title        :                                                        *
+ * Revision     : V1.0                                                   *
+ * Notes        :                                                        *
+ * --------------------------------------------------------------------- *
+ * Revision History:                                                     *
+ *   When             Who         Revision       Description of change   *
+ * -----------    -----------    ---------      ------------------------ *
+ * 2-15-2016      charlie_weng     V1.0          Created the program     *
+ *                                                                       *
+\*************************************************************************/
+'use strict';
+
+var debug  = require('debug')('ledmq:devdbapi');
+
+///////////////////////////////////////////////////////////////////////////
+var devStats = {};
+var devToken = {};
+
+///////////////////////////////////////////////////////////////////////////
+var getNodeId = function( did, fn ){ 
+       
+    if( devStats[ did ] ){   
+        var nodeid = devStats[ did ].nodeid; 
+        fn( null, nodeid );        
+    }
+    else{
+        fn( null, null );
+    } 
+}
+
+///////////////////////////////////////////////////////////////////////////
+var putDevice = function( did, devInfo, fn ){ 
+    devStats[ did ] = devInfo;
+    fn( null, 'ok' );
+}
+
+///////////////////////////////////////////////////////////////////////////
+var delDevice = function( did, fn ){ 
+    delete  devStats[ did ];
+    fn( null, 'ok' );
+}
+
+///////////////////////////////////////////////////////////////////////////
+var getDevToken = function( fn ){ 
+    var data = {index: [], items: {}};
+    for( var p in devToken ){
+		data.index.push(p);
+		data.items[i] = devToken[p];
+	}
+    fn( null, data );
+}
+
+////////////////////////////////////////////////////////////////////////////
+var setDevToken = function( gid, token, fn ){ 
+          
+    debug('set Device token',gid,token);
+    if( gid && token ){ 
+        devToken[ gid ] = token;
+        fn( null, 'ok' );
+    }
+    else
+    {
+        fn( 'err', null );
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+var getAllDev = function( fn ){ 
+    var data = {index: [], items: {}};
+    for( var p in devStats ){
+        data.index.push(p);
+		data.items[p] = devStats[p];
+    }
+    fn( null, data );
+}
+
+/////////////////////////////////////////////////////////////////////////////
+module.exports = {
+    getNodeId   : getNodeId,
+    putDevice   : putDevice,
+    delDevice   : delDevice,
+    getDevToken : getDevToken,
+    setDevToken : setDevToken,
+    getAllDev   : getAllDev
+}
